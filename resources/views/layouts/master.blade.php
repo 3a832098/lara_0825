372d23d2e7226c8856282d2bc30cf88217963151
GUO CHIH-LING <3a832098@gm.student.ncut.edu.tw>
2021-12-06 21:42:13 +0800

    練習3-e 使用asset()方法修改js檔案參考位置

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -44,11 +44,11 @@
     @include('layouts.partials.footer')
 
   <!-- Bootstrap core JavaScript -->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="{{asset('js/jquery.min.js')}}"></script>
+  <script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
 
   <!-- Custom scripts for this template -->
-  <script src="js/clean-blog.min.js"></script>
+  <script src="{{asset('js/clean-blog.min.js')}}"></script>
 
 </body>
 
