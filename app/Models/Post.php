3d4fa9632a7799b6c15b88ed50f48edf62305baa
GUO CHIH-LING <3a832098@gm.student.ncut.edu.tw>
2021-12-07 01:42:59 +0800

    練習7-2 在post.php增加和comment的關係

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Comment;
 
 class Post extends Model
 {
@@ -15,4 +16,10 @@ class Post extends Model
       'content',
       'is_feature'
     ];
+
+    public function post()
+    {
+        return $this-> belongsTo(Post::class);
+
+    }
 }
