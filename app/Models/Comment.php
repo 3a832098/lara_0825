c165ff379e2bbc54c42730f63cbf050db66689a5
GUO CHIH-LING <3a832098@gm.student.ncut.edu.tw>
2021-12-07 01:45:01 +0800

    練習7-3 在comment.php增加和post.php的關係

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,9 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Post;
 
 class Comment extends Model
 {
     use HasFactory;
     protected  $table = 'comments';/*可略*/
+
+    public function post()
+    {
+
+        return $this->belongsTo(Post::class);
+
+    }
 }
